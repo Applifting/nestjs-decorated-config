8df0bdd806c47a10eb2f1971a78cb862a868d910
Ota Vlna <ota.vlna@applifting.cz>
2023-10-17 19:53:51 +0200

    feat: configuration

--- lib/config.module.ts
+++ lib/config.module.ts
@@ -2,18 +2,46 @@ import { DynamicModule, Global, Module } from '@nestjs/common';
 import { ConfigPrinter } from './utils/config-printer';
 import { ConfigValidator } from './utils/config-validator';
 
+type ConfigModuleOptions = {
+  /**
+   * False by default
+   */
+  printOnStartup?: boolean;
+  /**
+   * True by default
+   */
+  validate?: boolean;
+};
+
 @Global()
 @Module({})
 export class ConfigModule {
-  static forRoot(ConfigService: {
-    new (): Record<string, any>;
-  }): DynamicModule {
+  static async forRootAsync(
+    ConfigService: {
+      new (): Record<string, any>;
+    },
+    options: ConfigModuleOptions,
+  ): Promise<DynamicModule> {
+    const optionsWithDefaults = {
+      printOnStartup: options.printOnStartup ?? false,
+      validate: options.validate ?? true,
+    };
+
     const config = new ConfigService();
     const configPrinter = new ConfigPrinter();
     const configValidator = new ConfigValidator();
 
-    configPrinter.print(config);
-    configValidator.validate(config);
+    if (optionsWithDefaults.printOnStartup) {
+      configPrinter.print(config);
+    }
+    if (optionsWithDefaults.validate) {
+      try {
+        await configValidator.validate(config);
+      } catch (e) {
+        console.error(`Config validation failed: ${e}`);
+        process.exit(1);
+      }
+    }
 
     return {
       module: ConfigModule,
